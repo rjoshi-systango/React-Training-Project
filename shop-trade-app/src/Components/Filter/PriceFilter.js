5be85e798adf45e37b9f008afe148c4cc2546168
rjoshi-systango <rjoshi@systango.com>
2022-08-24 13:03:53 +0530

    fix: price-filter-ui

--- shop-trade-app/src/Components/Filter/PriceFilter.js
+++ shop-trade-app/src/Components/Filter/PriceFilter.js
@@ -8,7 +8,7 @@ const PriceFilter = () => {
     return (
 
         <div className={"classes.price_filter"}>
-            <button className={classes.button}>Sort By:
+            <button className={classes.button}>Sort By : 
                 <select name="" id="" className={classes.select_option} onChange={categoryChangeHandler}>
                     <option value="low-to-high">Price Low To High</option>
                     <option value="high-to-low">Price High To Low</option>
