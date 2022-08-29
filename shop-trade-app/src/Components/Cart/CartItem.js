a7c641d1d49ff9d4997d2e12ecbdf873df853fc4
rjoshi-systango <rjoshi@systango.com>
2022-08-29 15:16:40 +0530

    feature-select-all-patial-complete

--- shop-trade-app/src/Components/Cart/CartItem.js
+++ shop-trade-app/src/Components/Cart/CartItem.js
@@ -1,54 +1,63 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus, faMinus, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faMinus, faTrash, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { deleteCartProduct, updateProductQuanity } from "../../Store/product-slice";
 import { useDispatch } from "react-redux";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import classes from './CartItem.module.css';
 
 const CartItem = (props) => {
-  const { productInformation } = props;
-  // console.log(productInformation);
+  const { productInformation, isSelectAllClicked } = props;
   let isQuantityOne = productInformation.quantity === 1;
-  const [isCheckboxClicked, setIsCheckboxClicked] = useState(false);
+  const [isCheckboxClicked, setIsCheckboxClicked] = useState(isSelectAllClicked);
+  // const [isCheckboxTrue, setIsCheckboxTrue] = useState(false);
   const dispatch = useDispatch();
-  // console.log(productInformation);
 
+  useEffect(() => {
+    if (isSelectAllClicked) {
+      setIsCheckboxClicked(isSelectAllClicked);
+    }
+    else if(!isSelectAllClicked && !isCheckboxClicked){
+      setIsCheckboxClicked(false);
+    }
+    else {
+      setIsCheckboxClicked(false);
+    }
   
-  
-  
+  }, [isSelectAllClicked, isCheckboxClicked]);
+
   const minusClickHandler = (event) => {
-    if(productInformation.quantity > 1) {
+    if (productInformation.quantity > 1) {
       dispatch(updateProductQuanity(productInformation.firebaseId, (productInformation.quantity - 1)));
-      const totalPrice = productInformation.price ;
+      const totalPrice = productInformation.price;
       console.log(totalPrice);
-      if(isCheckboxClicked) {
+      if (isCheckboxClicked) {
         console.log("yes plus if checked");
         props.calculateTotalPrice("SUB", totalPrice);
       }
     }
   }
-  
+
   const deleteClickHandler = () => {
     console.log(productInformation);
     console.log(productInformation.firebaseId);
     dispatch(deleteCartProduct(productInformation.firebaseId));
     const totalPrice = productInformation.price * productInformation.quantity;
-    if(isCheckboxClicked) {
+    if (isCheckboxClicked) {
       props.calculateTotalPrice("SUB", totalPrice);
     }
-    
+
   }
-  
+
   const plusClickHandler = () => {
     dispatch(updateProductQuanity(productInformation.firebaseId, (productInformation.quantity + 1)));
-    const totalPrice = productInformation.price ;
-      if(isCheckboxClicked) {
-        props.calculateTotalPrice("ADD", totalPrice);
-      }
-
-    
+    const totalPrice = productInformation.price;
+    if (isCheckboxClicked) {
+      props.calculateTotalPrice("ADD", totalPrice);
+    }
   }
-  
+
   const checkboxChangeHandler = (event) => {
+    // console.log(isCheckboxClicked);
     setIsCheckboxClicked((state) => !state);
     const checked = event.target.checked;
     const totalPrice = productInformation.price * productInformation.quantity;
@@ -58,7 +67,14 @@ const CartItem = (props) => {
   return (
     <tbody>
       <tr>
+        <td className="align-middle">
+          <div >
+            <FontAwesomeIcon className={classes.deleteCrossIcon} onClick={deleteClickHandler} icon={faXmark} />
+
+          </div>
+        </td>
         <th scope="row">
+
           <div className="d-flex align-items-center">
             <img src={productInformation.image_src[0]} className="img-fluid rounded-3"
               style={{ width: "120px" }} alt="Book" />
@@ -69,31 +85,37 @@ const CartItem = (props) => {
           </div>
         </th>
         <td className="align-middle">
-          <input type="checkbox" className="mb-0" style={{ fontWeight: "500" }} onChange={checkboxChangeHandler}/>
+          <input type="checkbox" className="mb-0" checked={isCheckboxClicked} style={{ fontWeight: "500"}} onChange={checkboxChangeHandler} />
         </td>
         <td className="align-middle">
           <p className="mb-0" style={{ fontWeight: "500" }}>{productInformation.vendor}</p>
         </td>
+        {/* <td>
+          <div>
+            <FontAwesomeIcon icon={fapinner} />
+          </div>
+        </td> */}
         <td className="align-middle">
           <div className="d-flex flex-row">
-            {isQuantityOne && 
+            {isQuantityOne &&
               <button className="btn btn-link px-2"
                 onClick={deleteClickHandler} >
-                <FontAwesomeIcon icon={faTrash} />
+                <FontAwesomeIcon className={classes.icon} icon={faTrash} />
               </button>
             }
-             {!isQuantityOne && 
+            {!isQuantityOne &&
               <button className="btn btn-link px-2"
                 onClick={minusClickHandler} >
-                <FontAwesomeIcon icon={faMinus} />
+                <FontAwesomeIcon className={classes.icon} icon={faMinus} />
               </button>
             }
-           
+
             <p className="form-control form-control-sm " style={{ width: "50px" }}>{productInformation.quantity}</p>
 
-            <button className="btn btn-link px-2 "
+            <button className="btn btn-link px-2  "
               onClick={plusClickHandler} >
-              <FontAwesomeIcon icon={faPlus} />
+              <FontAwesomeIcon className={classes.icon} icon={faPlus} />
+
             </button>
           </div>
         </td>
--- shop-trade-app/src/Components/Cart/index.js
+++ shop-trade-app/src/Components/Cart/index.js
@@ -6,14 +6,16 @@ const priceReducer = (state, action) => {
         return { 
             subTotal : state.subTotal +  action.productTotalPrice, 
             tax: state.tax +  (action.productTotalPrice * 5) / 100, 
-            total: state.total + (action.productTotalPrice + (action.productTotalPrice * 5) / 100)
+            total: state.total + (action.productTotalPrice + (action.productTotalPrice * 5) / 100),
+            isSelectAllClicked: state.isSelectAllClicked 
          }
     }
     if(action.type === false) {
         return { 
             subTotal : state.subTotal -  action.productTotalPrice, 
             tax: state.tax -  (action.productTotalPrice * 5) / 100, 
-            total: state.total - (action.productTotalPrice + (action.productTotalPrice * 5) / 100)
+            total: state.total - (action.productTotalPrice + (action.productTotalPrice * 5) / 100),
+            isSelectAllClicked: state.isSelectAllClicked ? !state.isSelectAllClicked : state.isSelectAllClicked
          }
     }
     if(action.type === "ADD") {
@@ -23,7 +25,8 @@ const priceReducer = (state, action) => {
         return { 
             subTotal : state.subTotal +  productPrice, 
             tax: state.tax +  (productPrice * 5) / 100, 
-            total: state.total + (productPrice + (productPrice * 5) / 100)
+            total: state.total + (productPrice + (productPrice * 5) / 100),
+            isSelectAllClicked: state.isSelectAllClicked
          }
     }
     if(action.type === "SUB") {
@@ -32,38 +35,54 @@ const priceReducer = (state, action) => {
         return { 
             subTotal : state.subTotal -  productPrice, 
             tax: state.tax -  (productPrice * 5) / 100, 
-            total: state.total - (productPrice + (productPrice * 5) / 100)
+            total: state.total - (productPrice + (productPrice * 5) / 100),
+            isSelectAllClicked: state.isSelectAllClicked
          }
     }
-
+    if (action.type === "SELECT_ALL") {
+        const allProductTotal = action.totalPrice;
+        return {
+            subTotal: allProductTotal,
+            tax: (allProductTotal * 5) / 100,
+            total: allProductTotal + ((allProductTotal * 5) / 100),
+            isSelectAllClicked : !state.isSelectAllClicked
+        }
+    }
+    if (action.type === "DESELECT_ALL") {
+        return {
+            subTotal: 0,
+            tax: 0,
+            total: 0,
+            isSelectAllClicked: !state.isSelectAllClicked
+        }
+    }
 }
 
 const Cart = (props) => {
     const { productList } = props;
-    // console.log(productList);
-    const [totalPrice, dispatch] = useReducer(priceReducer, {total: 0, tax: 0, subTotal: 0});
-    // console.log(totalPrice);
-    // const [totalBill, setTotalBill] = useState(0);
-    // const [subTotal, setSubTotal] = useState(0);
-    // const [totalTax, setTotalTax] = useState(0);
+    const [totalPrice, dispatch] = useReducer(priceReducer, {total: 0, tax: 0, subTotal: 0, isSelectAllClicked : false});
 
     const calculateTotalPrice = (checkbox, productTotalPrice) => {
-      
-        
-            dispatch({type: checkbox, productTotalPrice});
-            // setSubTotal((state) => state + productTotalPrice);
-            // console.log(subTotal);
-            // setTotalTax((state) => state + (productTotalPrice * 5) / 100);
-            // setTotalBill((state) => state + (subTotal + totalTax));
-            // console.log(totalBill);
-      
-            // setSubTotal((state) => state - productTotalPrice);
-            // console.log(productTotalPrice);
-            // setTotalTax((state) => state - (productTotalPrice * 5) / 100);
-            // setTotalBill((state) => state - (subTotal + totalTax));
+        dispatch({type: checkbox, productTotalPrice});
+    }
 
-        
-     
+    const selectAllChangeHandler = (event) => {
+        // console.log(event.target.checked);
+        const checkbox = event.target.checked;
+
+        if (checkbox) {
+            let totalPrice = 0; 
+            productList.forEach((product) => {
+                totalPrice += product.price * product.quantity
+            })
+            dispatch({type: "SELECT_ALL", totalPrice});
+        }
+        else {
+            dispatch({type: "DESELECT_ALL"});
+        }
+
+        // console.log(totalPrice.isSelectAllClicked);
+
     }
 
     return (
@@ -71,14 +90,15 @@ const Cart = (props) => {
             <div className="container h-100 py-5">
                 <div className="row d-flex justify-content-center align-items-center h-100">
                     <div className="col">
-
                         <div className="table-responsive">
                             <table className="table">
                                 <thead>
                                     <tr>
+                                        <th scope="col"> </th>
                                         <th scope="col" className="h5">Systango Shopping Bag</th>
-                                        <th scope="col">  
-                                            <input type="checkbox"/> Select</th>
+                                        <th scope="col">
+                                            <input type="checkbox" checked={totalPrice.isSelectAllClicked} onChange={selectAllChangeHandler}/> Select
+                                        </th>
                                         <th scope="col">Brand</th>
                                         <th scope="col">Quantity</th>
                                         <th scope="col">Price ($)</th>
@@ -87,52 +107,39 @@ const Cart = (props) => {
                                 {/* {tbody} */}
                                 {
                                     productList.map((product) => (
-
                                         <CartItem key={`${product.id} ${product.sizeId}`}
                                             productInformation={product}
                                             calculateTotalPrice={calculateTotalPrice}
+                                            isSelectAllClicked={totalPrice.isSelectAllClicked}
                                         />
                                     ))
-
                                 }
-
                             </table>
                         </div>
 
                         <div className="card shadow-2-strong mb-5 mb-lg-0" style={{ borderRadius: "16px" }} >
                             <div className="card-body p-4">
-
                                 <div className="row">
-
-
                                     <div className="col-lg-4 col-xl-3">
                                         <div className="d-flex justify-content-between" style={{ fontWeight: "500" }}>
                                             <p className="mb-2">Subtotal</p>
-                                            <p className="mb-2">${totalPrice.subTotal}</p>
+                                            <p className="mb-2">${totalPrice.subTotal.toFixed(2)}</p>
                                         </div>
-
-
-
                                         <hr className="my-4" />
-
                                         <div className="d-flex justify-content-between mb-4" style={{ fontWeight: "500" }}>
                                             <p className="mb-2">Total (tax included)</p>
-                                            <p className="mb-2">${totalPrice.tax}</p>
+                                            <p className="mb-2">${totalPrice.tax.toFixed(2)}</p>
                                         </div>
-
                                         <button type="button" className="btn btn-primary btn-block btn-lg">
                                             <div className="d-flex justify-content-between">
                                                 <span>Checkout</span>
-                                                <span className="px-2"> ${totalPrice.total}</span>
+                                                <span className="px-2"> ${totalPrice.total.toFixed(2)}</span>
                                             </div>
                                         </button>
-
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
